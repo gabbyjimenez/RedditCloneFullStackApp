0d31dc29036019ba9e3f61210590236799a8124a
Cole Perry <colepuro@outlook.com>
2024-08-03 16:11:42 -0400

    push partial front

---
+++ vue/src/components/DenList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="topic" v-for="den in dens" v-bind:key="den.id">
+    <router-link v-bind:to="{ name: 'TopicDetailsView', params: { denId: den.id } }">
+      {{ den.title }}  
+    </router-link>
+  </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/src/components/SideBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <aside>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Portfolio</a></li>
+      <li><a href="#">Contact Us</a></li>
+    </ul>
+
+
+  </aside>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+#siteTitle {
+  text-align: left;
+}
+
+a {
+  margin-left: 5px;
+}
+
+img {
+  width: 30%;
+  height: auto;
+}
+
+aside {
+  position: fixed;
+  /* left: 0; 
+    top: 0; 
+    padding-top: 20px;  */
+  border-right: 1px solid rgba(0, 173, 238, 0.4);
+  width: 200px;
+  height: 100vh;
+}
+
+</style>
