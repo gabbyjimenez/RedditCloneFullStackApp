0188a4173aef96e61ff2e93efc5cedf9c228e037
Carter Kistler <carter.kistler25@gmail.com>
2024-08-05 15:08:11 -0400

    added filter by category functionality for dens

--- vue/src/components/DenDetails.vue
+++ vue/src/components/DenDetails.vue
@@ -1,50 +1,60 @@
 <template>
-    <div>
-        <label for="">Search: </label>
-        <input type="text" name="denName" v-model="searchFilter">
-    </div>
+  <div>
+    <label for="">Search: </label>
+    <input type="text" name="denName" v-model="searchFilter" />
+  </div>
 
-    <div>
-        <h4>Explore Dens</h4>
-        <div class="den" v-for="den in filteredDens" v-bind:key="den.denName" >
-        <p v-on:click="$router.push({ name: 'den', params: { denName: den.denName } })">{{ den.denId }} : {{ den.denName }} - {{ den.denCreatorUserName }}</p>
-        </div>
+  <div>
+    <h4>Explore Dens</h4>
+    <div class="den" v-for="den in filteredDens" v-bind:key="den.denName">
+      <p
+        v-on:click="
+          $router.push({ name: 'den', params: { denName: den.denName } })
+        "
+      >
+        {{ den.denId }} : {{ den.denName }} - {{ den.denCreatorUserName }}
+      </p>
     </div>
+  </div>
 </template>
   
 <script>
-import DenList from './DenList.vue';
-
-
+import DenList from "./DenList.vue";
 
 export default {
-
-    props: {
-       dens: {
-        type: Array,
-        
-       }
-
+  props: {
+    dens: {
+      type: Array,
     },
-    
-    data() {
-        return {
-            searchFilter: ''
-        }
+  },
+
+  data() {
+    return {
+      searchFilter: "",
+    };
+  },
+  computed: {
+    filteredDens() {
+      // return this.dens.filter((den) => {
+      //     return this.searchFilter == '' ? true : den.denName.toLowerCase().includes(this.searchFilter.toLowerCase()) || den.categoryNames.toLowerCase().includes(this.searchFilter.toLowerCase());
+      // });
+
+      const searchFilter = this.searchFilter.toLowerCase();
+      return this.dens.filter((den) => {
+        // Check if denName includes the searchFilter
+        const nameMatch = den.denName.toLowerCase().includes(searchFilter);
+
+        // Check if any categoryName includes the searchFilter
+        const categoryMatch = den.categoryNames.some((category) =>
+          category.toLowerCase().includes(searchFilter)
+        );
+
+        // Return true if either condition matches or searchFilter is empty
+        return searchFilter === "" ? true : nameMatch || categoryMatch;
+      });
     },
-    computed: {
-        filteredDens() {
-            return this.dens.filter((den) => {
-                return this.searchFilter == '' ? true : den.denName.includes(this.searchFilter) || den.categoryNames.includes(this.searchFilter);
-            });
-
-
-
-        }
-    },
-  
-
-}
+  },
+};
 </script>
   
 <style>
