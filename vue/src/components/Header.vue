c95d407c86edb33e749c857862fee50e0ce1554c
Nicholas Eagle[D[C[D[D[D[D[C[C[C[C[D[D[C[D[C[D[C[D[C[D[C[C[D[C[D[C <nicholaseagle1@gmail.com>
2024-08-06 11:46:49 -0400

    small changes

--- vue/src/components/Header.vue
+++ vue/src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <header>
-      <router-link id="homeLink" v-bind:to="{ name: 'home' }">Home</router-link>
+      <router-link id="homeLink" v-bind:to="{ name: 'home' }"><img src="https://res.cloudinary.com/daprq6s7w/image/upload/v1722957878/foxbutton_uilkmi.png" /></router-link>
         <p id="headerText">Header Name</p>
        
         <p>favorite|logout</p>
