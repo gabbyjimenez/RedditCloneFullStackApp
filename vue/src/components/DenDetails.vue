c8898f29e9a580bae29738c463f4d43d87c7ee3f
Nicholas Eagle[D[C[D[D[D[D[C[C[C[C[D[D[C[D[C[D[C[D[C[D[C[C[D[C[D[C <nicholaseagle1@gmail.com>
2024-08-06 17:36:33 -0400

    card desc added

--- vue/src/components/DenDetails.vue
+++ vue/src/components/DenDetails.vue
@@ -15,8 +15,7 @@
         ">
         <h5 class="card-title">{{ den.denName }} {{ den.denCreatorUserName }}</h5>
         <p2 class="card-subtitle mb-2 text-muted">User ID: {{ den.denId }}</p2>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-          content.</p>
+        <p class="card-text">{{den.denDesc}}</p>
 
       </div>
     </div>
