6082d6331e15cf3ec5f0b346c913cf40700e67f0
Cole Perry <colepuro@outlook.com>
2024-08-07 10:08:11 -0400

    pull

--- vue/src/components/PostList.vue
+++ vue/src/components/PostList.vue
@@ -1,12 +1,10 @@
 <template>
-
-
     <div>
         <label for="">Search: </label>
         <input type="text" name="denName" v-model="searchFilter">
     </div>
 
-    <div class="den" v-for="post in filteredPosts" v-bind:key="post.postTitle" >
+    <div class="den" v-for="post in filteredPosts" v-bind:key="post.postTitle">
         <div v-on:click="$router.push({ name: 'post', params: { postId: post.postId } })" v-bind="post">
             <label>{{ post.postTitle }} : {{ post.creatorUsername }}</label>
             <p>Desc</p>
@@ -47,24 +45,21 @@ export default {
     },
     methods: {
 
-
         DeletePost(post) {
-      console.log("bleep")
-      if (confirm("Are you sure you want to delete this message? This action cannot be undone.")) {
+            console.log("bleep")
+            if (confirm("Are you sure you want to delete this message? This action cannot be undone.")) {
 
-        // TODO - Do a delete, then navigate to Topic Details on success
-        // For errors, call handleErrorResponse
-        console.log(post)
-        console.log("blorg")
+                console.log(post)
+                console.log("blorg")
 
-        PostService.deletePost(post).then(response => {
-          console.log("deleted")
-        }).catch(error => {
-          this.handleErrorResponse(error, 'deleting');
-        })
+                PostService.deletePost(post).then(response => {
+                    console.log("deleted")
+                }).catch(error => {
+                    this.handleErrorResponse(error, 'deleting');
+                })
 
-      }
-    }
+            }
+        }
     }
 
 }
@@ -78,5 +73,4 @@ export default {
     background-color: #f2f2f2;
     cursor: pointer;
 }
-
 </style>
