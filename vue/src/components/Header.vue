202edeee7997c785c2cb71ce21838fffc4010b04
Cole Perry <colepuro@outlook.com>
2024-08-08 17:13:50 -0400

    push

--- vue/src/components/Header.vue
+++ vue/src/components/Header.vue
@@ -3,10 +3,10 @@
     <router-link id="homeLink" v-bind:to="{ name: 'home' }"><img
         src="https://res.cloudinary.com/daprq6s7w/image/upload/v1722965836/foxbutton_j6yfhm.png" /></router-link>
     <div id="searchContainer">
-    <div  class="input-group rounded" id ="searchBar">
+    <!-- <div  class="input-group rounded" id ="searchBar">
       <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search"
         aria-describedby="search-addon" />
-    </div>
+    </div> -->
     </div>
     <div id="favLog">
     
