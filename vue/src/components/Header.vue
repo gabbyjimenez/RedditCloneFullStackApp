7a16154623d699b8a748e391b2202f2c52320129
Nicholas Eagle[D[C[D[D[D[D[C[C[C[C[D[D[C[D[C[D[C[D[C[D[C[C[D[C[D[C <nicholaseagle1@gmail.com>
2024-08-12 16:43:21 -0400

    logout modal

--- vue/src/components/Header.vue
+++ vue/src/components/Header.vue
@@ -4,74 +4,29 @@
     <router-link v-bind:to="{ name: 'home' }"><img
         src="https://res.cloudinary.com/daprq6s7w/image/upload/v1723474352/foxtrot_logo_fin_noText_zwlfxu.svg" /></router-link>
  </div>
-<!--   
-    
+<!--
+
     <router-link id="LoginLink" v-bind:to="{ name: 'login' }">Login |</router-link>
     <router-link id="LoginLink" v-bind:to="{ name: 'logout' }"> Logout</router-link> -->
 
 
     <div id="favLog">
-      <!-- <router-link v-if="this.$store.state.user.username != null" v-bind:to="{ name: 'logout' }" class="auth-link">
+      <router-link v-if="this.$store.state.user.username != null" v-bind:to="{ name: 'logout' }" class="auth-link">
         Logout
-      </router-link> -->
-
-<!-- modal test -->
-<p v-if="this.$store.state.user.username != null" v-bind:to="{ name: 'logout' }" class="auth-link" data-toggle="modal" data-target="#logoutModal">
-        Logout
-</p>
-
-
-<!-- modal test -->
-
+      </router-link>
       <router-link v-else v-bind:to="{ name: 'login' }" class="auth-link">
         Login
       </router-link>
     </div>
   </header>
-
-  <!-- test modal -->
- <!-- Button trigger modal -->
-
-
-<!-- Modal -->
-<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" Segoe-labelledby="logoutModalLabel" Segoe-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="logoutModalLabel">Are you sure?</h5>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" v-on:click="logout" data-dismiss="modal">Continue</button>
-        <button type="button" class="btn btn-primary"  data-dismiss="modal">Cancel</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-  <!-- test modal -->
 </template>
 
 <script>
-export default {
-
-
-  methods: {
-
-    logout(){
-      this.$store.commit("LOGOUT");
-      this.$router.push("/dens");
-    },
-    goHome(){
-      this.$router.push("/")
-    }
-    
-  },
-  }
-  
+export default {}
 </script>
 
 <style scoped>
-/* Apply a professional font */
+ /* Apply a professional font  */
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
 
 header {
@@ -87,7 +42,7 @@ header {
 #homeLink {
   display: flex;
   align-items: center;
-  
+
   height: 8%;
 
 
@@ -107,10 +62,10 @@ img {
 }
 
 #headerText {
-  font-family: 'Roboto', sans-serif; /* Professional font */
-  font-weight: 700; /* Bold text */
-  color: #fff; /* White text for contrast */
-  font-size: 1.5rem; /* Adjust size as needed */
+  font-family: 'Segoe UI';
+  font-weight: 700;
+  color: rgba(252,202,70,1);
+  font-size: 1.5rem;
 }
 
 #favLog {
@@ -119,8 +74,8 @@ img {
 }
 
 .auth-link {
-  color: #fff;
-  text-decoration: none; /* Remove underline from links */
+  color:rgba(252,202,70,1);
+  text-decoration: none;
   margin-left: 1rem;
   padding: 0.5rem 1rem;
   border-radius: 4px;
