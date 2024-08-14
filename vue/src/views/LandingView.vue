278f9e27f699488c30039c11af6d13fea51a1883
Nicholas Eagle[D[C[D[D[D[D[C[C[C[C[D[D[C[D[C[D[C[D[C[D[C[C[D[C[D[C <nicholaseagle1@gmail.com>
2024-08-14 13:55:55 -0400

    worked on landing page

--- vue/src/views/LandingView.vue
+++ vue/src/views/LandingView.vue
@@ -30,18 +30,18 @@
         <!-- Content Section -->
         <div id="content" class="w3-row-padding w3-center w3-margin-top">
           <BenefitsCard 
-            title="Responsive Design"
+            title="Enhanced Community Engagement"
             icon="fa-light fa-network-wired"
             :benefits="['Built-in responsiveness', 'Mobile first fluid grid', 'Fits any screen sizes', 'PC Tablet and Mobile']"
           />
           <BenefitsCard 
-            title="Fast Performance"
-            icon="fa-rocket"
+            title="Streamlined Information Sharing"
+            icon="fa-solid fa-comments"
             :benefits="['Optimized for speed', 'Minimal load times', 'Efficient data handling', 'Lightning-fast interactions']"
           />
           <BenefitsCard 
-            title="Secure"
-            icon="fa-lock"
+            title="Inclusive and Collaborative Environment"
+            icon="fa-solid fa-comments"
             :benefits="['Top-notch security', 'Data encryption', 'Regular updates', '24/7 monitoring']"
           />
         </div>
