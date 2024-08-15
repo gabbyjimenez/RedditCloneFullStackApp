a7010b855a631e020220a2e8086d188f49d9ca72
Gabriela Jimenez <gabbyjimnz@gmail.com>
2024-08-15 01:10:28 -0400

    Added title + 'back to dens' button from DenView

--- vue/src/views/DenView.vue
+++ vue/src/views/DenView.vue
@@ -1,9 +1,23 @@
 <template>
   <div>
-    <theHeader id="header" />
+    <!-- referencing DenName that is pulled from URL -->
+    <theHeader id="header" :showBackToDensButton="true" />
+
+    <!-- DEN NAME TITLE -->
+    <h1>{{ denName }}</h1>
+
+
     <AddPostForm id="addPostForm" />
     <PostList/>
-    
+
+     <!-- THIS IS THE 'Back to Dens' BUTTON  -->
+    <div class="button-container">
+      <router-link v-bind:to="{ name: 'dens' }">
+        <button id="backToDensListButton" type="button" class="btn btn-primary">
+          Back to Dens List
+        </button>
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -25,7 +39,6 @@ export default {
       denName: this.$route.params.denName,
     };
   },
-
   methods: {
 
     getPosts(name) {
@@ -62,4 +75,18 @@ div {
 padding: 1%;
 
 }
+/* THIS MAKE THE 'Back to Dens' button go to the center */
+.button-container {
+  display: flex;
+  justify-content: center; /* Center horizontally */
+  margin-top: 1em; /* Optional: Add space above the button */
+}
+
+/* COLOR OF Back To Dens List button */
+#backToDensListButton {
+  background-color: #619c8a;
+  border-color: #619c8a;
+  color: white;
+}
+
 </style>
